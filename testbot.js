f2ffe89a205604876b45f90bda15fd1c7f3043bb
SWERXii <SWERXii@users.noreply.github.com>
2018-12-11 16:52:03 -0300

    update #3

--- testbot.js
+++ testbot.js
@@ -26,14 +26,17 @@ client.on('message', message => {
     } catch (e) {
         console.log(e.stack);
     }
-    if (message.author.bot) return;
+
+    if (sender.id === '522092594726174721') {
+         return;
+    }
     if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA')))) {
         message.channel.send(message.author + ' _has been banned due to tosic behaviour_')
     }
     if (msg.includes('NIGERIA'))
         message.author.send('Reporta2 al INADI')
-
-});
+    
+    });
 client.login(process.env.BOT_TOKEN);
 
 // Ready event
