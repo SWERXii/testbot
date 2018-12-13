0343c629b8f3f94fc9ceea3bd8eeb74559d9586c
SWERXii <45774276+SWERXii@users.noreply.github.com>
2018-12-13 18:18:22 -0300

    Update testbot.js

--- testbot.js
+++ testbot.js
@@ -1,44 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const botConfig = require('./botConfig.json');
 
-// Constant variables
-const prefix = '!';
-const ownerID = '263493405684924416';
+client.on('ready', () => {
 
-// Listener events
+    console.log("The bot is logged in.");
+});
+
 client.on('message', message => {
-    // Vars
-    let args = message.content.slice(prefix.length).trim().split(' ');
-    let cmd = args.shift().toLocaleLowerCase();
+
     var sender = message.author;
     var msg = message.content.toLocaleUpperCase();
+    let inclusive = ['xs', 'todes', 'elles', 'nosotres', 'bobes', 'divines', 'dibines', 'otres', 'les'];
+    let foundInText = false;
+    let prefix = botConfig.prefix;
+    let cmd = messageArray = [0];
+    let args = messageArray.slice(1);
 
-    // Return statements
-    
-    if (message.author.bot) return;                     // Bot ignore
-    if (!message.content.startsWith(prefix)) return;    // No prefix ignore
-
-    // Command handler
-    try {
-        
-        let commandFile = require(`./commands/${cmd}.js`);
-        commandFile.run(client, message, args);
-        
-    } catch (e) {
-        console.log(e.stack);
+    if (cmd === `${prefix}ban`){
+        let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!bUser) return message.channel.send("Can't find user!");
+        if(bUser) {
+            message.channel.send(${bUser} + ' _has been banned by BattlEye™._');
+        }
     }
 
-    if (sender.id === '522092594726174721') {
+    }
+    for (var i in inclusive){
+        if (message.content.toLowerCase().includes(inclusive[i].toLowerCase())) foundInText = true;
+    }
+    if (foundInText) {
+        message.delete();
+        message.channel.send(message.author + '_ intentó escribir en idioma inclusive_');
+    }
+    if (sender.id === '521820814551678976') {
         return;
     }
-    if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA')))) {
+    if (msg.includes('NIGGA') || (msg.includes('NIGGER') || (msg.includes('NIBBA') || (msg.includes('IGGA'))))) {
         message.channel.send(message.author + ' _has been banned due to tosic behaviour_')
     }
-    if (msg.includes('NIGERIA'))
-        message.author.send('Reporta2 al INADI')
+    if (msg.includes('NIGERIA')){
+        message.author.send('Reporta2 al INADI');
+    }
     
-    });
+    
+});
 client.login(process.env.BOT_TOKEN);
-
-// Ready event
-client.on('ready', () => console.log('Launched!'));
